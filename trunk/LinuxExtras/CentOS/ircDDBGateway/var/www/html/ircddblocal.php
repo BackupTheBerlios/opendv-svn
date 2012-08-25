91c2a8b7201972131f83a4b42b82c72a0668f0a5
dl5di <dl5di@42a4f344-f963-4ded-a4f8-2595dd3c5695>
2012-08-25 08:29:41 +0000

    Configuration tools and scripts like used in Linux packages for CentOS/YUM and DebianWheezy/apt-get

---
+++ trunk/LinuxExtras/CentOS/ircDDBGateway/var/www/html/ircddblocal.php
@@ -0,0 +1,9 @@
+<?php
+$logPath='/var/log/dstar';
+$callsign='DB0LJ';
+$registerURL = 'http://dstar.prgm.org/dstar-reg.htm';
+$starLogPath = $logPath . '/STARnet.log';
+$linkLogPath = $logPath . '/Links.log';
+$hdrLogPath = $logPath . '/Headers.log';
+$gatewayConfigPath = '/etc/ircddbgateway';
+?>
