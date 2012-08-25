1735be0fd55698e730607960b432e8ad78aa1517
dl5di <dl5di@42a4f344-f963-4ded-a4f8-2595dd3c5695>
2012-08-25 08:42:10 +0000

    removed config file with existing settings, added template used by ircddbgw_conf

--- trunk/LinuxExtras/CentOS/ircDDBGateway/var/www/html/ircddblocal.php
+++
@@ -1,9 +0,0 @@
-<?php
-$logPath='/var/log/dstar';
-$callsign='DB0LJ';
-$registerURL = 'http://dstar.prgm.org/dstar-reg.htm';
-$starLogPath = $logPath . '/STARnet.log';
-$linkLogPath = $logPath . '/Links.log';
-$hdrLogPath = $logPath . '/Headers.log';
-$gatewayConfigPath = '/etc/ircddbgateway';
-?>
