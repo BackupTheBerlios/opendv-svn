cd4a4a42e2f907e9793328bed76cb300e2b58493
dl5di <dl5di@42a4f344-f963-4ded-a4f8-2595dd3c5695>
2012-09-04 23:13:01 +0000

    - several adjustments to paths and options

--- trunk/LinuxExtras/DebianWheezy/ircDDBGateway/usr/bin/ircddbgateway.sh
+++ trunk/LinuxExtras/DebianWheezy/ircDDBGateway/usr/bin/ircddbgateway.sh
@@ -1,3 +1,37 @@
 #!/bin/bash
-#LC_NUMERIC="en_US.UTF-8" su -c "/usr/bin/ircddbgateway -gui" opendv
-su -c "/usr/bin/ircddbgateway -gui" opendv
+#
+# ircDDBGateway start script for GUI
+# Hans-J. Barthen (DL5DI) - dl5di@gmx.de
+#
+# description: Starts the G4KLX ircDDBGateway in GUI mode and sets the LC_NUMERIC LOCALE
+# to en_US.UTF-8 to make the configfile compatible for GUI and dameon modem.
+# config: /etc/sysconfig/ircddbgateway
+#
+
+if [ $UID -ne 0 ]; then
+	echo
+	echo "ERROR:  This script must be run as the root user!"
+	echo "        Please use 'su' or log in as root and try again."
+	echo
+	exit 1
+fi
+
+NAME=ircddbgateway
+
+su -c "/usr/bin/$NAME -gui -logdir="/var/log/dstar" opendv &
+
+# Take care to start necessary services for DD-mode if enabled
+
+# Read configuration file if it is present
+[ -r /etc/default/$NAME ] && . /etc/default/$NAME || echo "Configfile /etc/default/$NAME is missing"
+
+if [ "${IRCDDBGATEWAY_DDENABLED}" = "yes" ];then
+    echo " - configure dd-mode network device $IRCDDBGATEWAY_DDDEV "
+    sleep 15
+    /sbin/ifconfig $IRCDDBGATEWAY_DDDEV $IRCDDBGATEWAY_DDIP netmask $IRCDDBGATEWAY_DDMASK
+    echo 1 > /proc/sys/net/ipv4/ip_forward
+    if [ "${IRCDDBGATEWAY_DDDHCP}" = "yes" ];then
+        echo " - start dhcpd "
+    	/etc/init.d/dhcpd start
+    fi
+fi
