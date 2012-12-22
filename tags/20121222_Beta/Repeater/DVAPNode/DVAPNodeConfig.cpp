95b109935fe247794b0f8de5b3e866967f84d068
naylorjs <naylorjs@42a4f344-f963-4ded-a4f8-2595dd3c5695>
2012-12-22 10:11:43 +0000

    The 20121222 beta release.

---
+++ tags/20121222_Beta/Repeater/DVAPNode/DVAPNodeConfig.cpp
@@ -0,0 +1,525 @@
+/*
+ *   Copyright (C) 2011,2012 by Jonathan Naylor G4KLX
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include "DVAPNodeConfig.h"
+
+const wxString  KEY_CALLSIGN           = wxT("callsign");
+const wxString  KEY_GATEWAY            = wxT("gateway");
+const wxString  KEY_MODE               = wxT("mode");
+const wxString  KEY_ACK                = wxT("ack");
+const wxString  KEY_RESTRICTION        = wxT("restriction");
+const wxString  KEY_RPT1_VALIDATION    = wxT("rpt1Validation");
+const wxString  KEY_GATEWAY_ADDRESS    = wxT("gatewayAddress");
+const wxString  KEY_GATEWAY_PORT       = wxT("gatewayPort");
+const wxString  KEY_LOCAL_ADDRESS      = wxT("localAddress");
+const wxString  KEY_LOCAL_PORT         = wxT("localPort");
+const wxString  KEY_DVAP_PORT          = wxT("dvapPort");
+const wxString  KEY_DVAP_FREQUENCY     = wxT("dvapFrequency");
+const wxString  KEY_DVAP_POWER         = wxT("dvapPower");
+const wxString  KEY_DVAP_SQUELCH       = wxT("dvapSquelch");
+const wxString  KEY_DVAP_OFFSET        = wxT("dvapOffset");
+const wxString  KEY_TIMEOUT            = wxT("timeout");
+const wxString  KEY_ACK_TIME           = wxT("ackTime");
+const wxString  KEY_BEACON_TIME        = wxT("beaconTime");
+const wxString  KEY_BEACON_TEXT        = wxT("beaconText");
+const wxString  KEY_BEACON_VOICE       = wxT("beaconVoice");
+const wxString  KEY_LANGUAGE           = wxT("language");
+const wxString  KEY_LOGGING            = wxT("logging");
+const wxString  KEY_WINDOW_X           = wxT("windowX");
+const wxString  KEY_WINDOW_Y           = wxT("windowY");
+
+const wxString     DEFAULT_CALLSIGN        = wxT("GB3IN  C");
+const wxString     DEFAULT_GATEWAY         = wxEmptyString;
+const DSTAR_MODE   DEFAULT_MODE            = MODE_SIMPLEX;
+const ACK_TYPE     DEFAULT_ACK             = AT_BER;
+const bool         DEFAULT_RESTRICTION     = false;
+const bool         DEFAULT_RPT1_VALIDATION = true;
+const wxString     DEFAULT_GATEWAY_ADDRESS = wxT("127.0.0.1");
+const unsigned int DEFAULT_GATEWAY_PORT    = 20010U;
+const wxString     DEFAULT_LOCAL_ADDRESS   = wxT("127.0.0.1");
+const unsigned int DEFAULT_LOCAL_PORT      = 20011U;
+const wxString     DEFAULT_DVAP_PORT       = wxEmptyString;
+const unsigned int DEFAULT_DVAP_FREQUENCY  = 145500000U;
+const int          DEFAULT_DVAP_POWER      = 10;
+const int          DEFAULT_DVAP_SQUELCH    = -100;
+const int          DEFAULT_DVAP_OFFSET     = 0;
+const unsigned int DEFAULT_TIMEOUT         = 180U;
+const unsigned int DEFAULT_ACK_TIME        = 500U;
+const unsigned int DEFAULT_BEACON_TIME     = 600U;
+const wxString     DEFAULT_BEACON_TEXT     = wxT("DVAP Node");
+const bool         DEFAULT_BEACON_VOICE    = false;
+const TEXT_LANG    DEFAULT_LANGUAGE        = TL_ENGLISH_UK;
+const bool         DEFAULT_LOGGING         = false;
+const int          DEFAULT_WINDOW_X        = -1;
+const int          DEFAULT_WINDOW_Y        = -1;
+
+
+#if defined(__WINDOWS__)
+
+CDVAPNodeConfig::CDVAPNodeConfig(wxConfigBase* config, const wxString& name) :
+m_config(config),
+m_name(wxT("/")),
+m_callsign(DEFAULT_CALLSIGN),
+m_gateway(DEFAULT_GATEWAY),
+m_mode(DEFAULT_MODE),
+m_ack(DEFAULT_ACK),
+m_restriction(DEFAULT_RESTRICTION),
+m_rpt1Validation(DEFAULT_RPT1_VALIDATION),
+m_gatewayAddress(DEFAULT_GATEWAY_ADDRESS),
+m_gatewayPort(DEFAULT_GATEWAY_PORT),
+m_localAddress(DEFAULT_LOCAL_ADDRESS),
+m_localPort(DEFAULT_LOCAL_PORT),
+m_port(DEFAULT_DVAP_PORT),
+m_frequency(DEFAULT_DVAP_FREQUENCY),
+m_power(DEFAULT_DVAP_POWER),
+m_squelch(DEFAULT_DVAP_SQUELCH),
+m_offset(DEFAULT_DVAP_OFFSET),
+m_timeout(DEFAULT_TIMEOUT),
+m_ackTime(DEFAULT_ACK_TIME),
+m_beaconTime(DEFAULT_BEACON_TIME),
+m_beaconText(DEFAULT_BEACON_TEXT),
+m_beaconVoice(DEFAULT_BEACON_VOICE),
+m_language(DEFAULT_LANGUAGE),
+m_logging(DEFAULT_LOGGING),
+m_x(DEFAULT_WINDOW_X),
+m_y(DEFAULT_WINDOW_Y)
+{
+	wxASSERT(config != NULL);
+
+	if (!name.IsEmpty())
+		m_name = wxT("/") + name + wxT("/");
+
+	long temp;
+
+	m_config->Read(m_name + KEY_CALLSIGN, &m_callsign, DEFAULT_CALLSIGN);
+
+	m_config->Read(m_name + KEY_GATEWAY, &m_gateway, DEFAULT_GATEWAY);
+
+	m_config->Read(m_name + KEY_MODE, &temp, long(DEFAULT_MODE));
+	m_mode = DSTAR_MODE(temp);
+
+	m_config->Read(m_name + KEY_ACK, &temp, long(DEFAULT_ACK));
+	m_ack = ACK_TYPE(temp);
+
+	m_config->Read(m_name + KEY_RESTRICTION, &m_restriction, DEFAULT_RESTRICTION);
+
+	m_config->Read(m_name + KEY_RPT1_VALIDATION, &m_rpt1Validation, DEFAULT_RPT1_VALIDATION);
+
+	m_config->Read(m_name + KEY_GATEWAY_ADDRESS, &m_gatewayAddress, DEFAULT_GATEWAY_ADDRESS);
+
+	m_config->Read(m_name + KEY_GATEWAY_PORT, &temp, long(DEFAULT_GATEWAY_PORT));
+	m_gatewayPort = (unsigned int)temp;
+
+	m_config->Read(m_name + KEY_LOCAL_ADDRESS, &m_localAddress, DEFAULT_LOCAL_ADDRESS);
+
+	m_config->Read(m_name + KEY_LOCAL_PORT, &temp, long(DEFAULT_LOCAL_PORT));
+	m_localPort = (unsigned int)temp;
+
+	m_config->Read(m_name + KEY_DVAP_PORT, &m_port, DEFAULT_DVAP_PORT);
+
+	m_config->Read(m_name + KEY_DVAP_FREQUENCY, &temp, long(DEFAULT_DVAP_FREQUENCY));
+	m_frequency = (unsigned int)temp;
+
+	m_config->Read(m_name + KEY_DVAP_POWER, &temp, long(DEFAULT_DVAP_POWER));
+	m_power = int(temp);
+
+	m_config->Read(m_name + KEY_DVAP_SQUELCH, &temp, long(DEFAULT_DVAP_SQUELCH));
+	m_squelch = int(temp);
+
+	m_config->Read(m_name + KEY_DVAP_OFFSET, &temp, long(DEFAULT_DVAP_OFFSET));
+	m_offset = int(temp);
+
+	m_config->Read(m_name + KEY_TIMEOUT, &temp, long(DEFAULT_TIMEOUT));
+	m_timeout = (unsigned int)temp;
+
+	m_config->Read(m_name + KEY_ACK_TIME, &temp, long(DEFAULT_ACK_TIME));
+	m_ackTime = (unsigned int)temp;
+
+	m_config->Read(m_name + KEY_BEACON_TIME, &temp, long(DEFAULT_BEACON_TIME));
+	m_beaconTime = (unsigned int)temp;
+
+	m_config->Read(m_name + KEY_BEACON_TEXT, &m_beaconText, DEFAULT_BEACON_TEXT);
+
+	m_config->Read(m_name + KEY_BEACON_VOICE, &m_beaconVoice, DEFAULT_BEACON_VOICE);
+
+	m_config->Read(m_name + KEY_LANGUAGE, &temp, long(DEFAULT_LANGUAGE));
+	m_language = TEXT_LANG(temp);
+
+	m_config->Read(m_name + KEY_LOGGING, &m_logging, DEFAULT_LOGGING);
+
+	m_config->Read(m_name + KEY_WINDOW_X, &temp, long(DEFAULT_WINDOW_X));
+	m_x = int(temp);
+
+	m_config->Read(m_name + KEY_WINDOW_Y, &temp, long(DEFAULT_WINDOW_Y));
+	m_y = int(temp);
+}
+
+CDVAPNodeConfig::~CDVAPNodeConfig()
+{
+	delete m_config;
+}
+
+#else
+
+CDVAPNodeConfig::CDVAPNodeConfig(const wxString& dir, const wxString& name) :
+m_fileName(),
+m_callsign(DEFAULT_CALLSIGN),
+m_gateway(DEFAULT_GATEWAY),
+m_mode(DEFAULT_MODE),
+m_ack(DEFAULT_ACK),
+m_restriction(DEFAULT_RESTRICTION),
+m_rpt1Validation(DEFAULT_RPT1_VALIDATION),
+m_gatewayAddress(DEFAULT_GATEWAY_ADDRESS),
+m_gatewayPort(DEFAULT_GATEWAY_PORT),
+m_localAddress(DEFAULT_LOCAL_ADDRESS),
+m_localPort(DEFAULT_LOCAL_PORT),
+m_port(DEFAULT_DVAP_PORT),
+m_frequency(DEFAULT_DVAP_FREQUENCY),
+m_power(DEFAULT_DVAP_POWER),
+m_squelch(DEFAULT_DVAP_SQUELCH),
+m_offset(DEFAULT_DVAP_OFFSET),
+m_timeout(DEFAULT_TIMEOUT),
+m_ackTime(DEFAULT_ACK_TIME),
+m_beaconTime(DEFAULT_BEACON_TIME),
+m_beaconText(DEFAULT_BEACON_TEXT),
+m_beaconVoice(DEFAULT_BEACON_VOICE),
+m_language(DEFAULT_LANGUAGE),
+m_logging(DEFAULT_LOGGING),
+m_x(DEFAULT_WINDOW_X),
+m_y(DEFAULT_WINDOW_Y)
+{
+	wxASSERT(!dir.IsEmpty());
+
+	wxString fileName = CONFIG_FILE_NAME;
+	if (!name.IsEmpty())
+		fileName = CONFIG_FILE_NAME + wxT("_") + name;
+
+	m_fileName.Assign(dir, fileName);
+
+	wxTextFile file(m_fileName.GetFullPath());
+
+	bool exists = file.Exists();
+	if (!exists)
+		return;
+
+	bool ret = file.Open();
+	if (!ret) {
+		wxLogError(wxT("Cannot open the config file - %s"), m_fileName.GetFullPath().c_str());
+		return;
+	}
+
+	long temp1;
+	unsigned long temp2;
+
+	wxString str = file.GetFirstLine();
+
+	while (!file.Eof()) {
+		if (str.GetChar(0U) == wxT('#')) {
+			str = file.GetNextLine();
+			continue;
+		}
+
+		int n = str.Find(wxT('='));
+		if (n == wxNOT_FOUND) {
+			str = file.GetNextLine();
+			continue;
+		}
+
+		wxString key = str.Left(n);
+		wxString val = str.Mid(n + 1U);
+
+		if (key.IsSameAs(KEY_CALLSIGN)) {
+			m_callsign = val;
+		} else if (key.IsSameAs(KEY_GATEWAY)) {
+			m_gateway = val;
+		} else if (key.IsSameAs(KEY_MODE)) {
+			val.ToLong(&temp1);
+			m_mode = DSTAR_MODE(temp1);
+		} else if (key.IsSameAs(KEY_ACK)) {
+			val.ToLong(&temp1);
+			m_ack = ACK_TYPE(temp1);
+		} else if (key.IsSameAs(KEY_RESTRICTION)) {
+			val.ToLong(&temp1);
+			m_restriction = temp1 == 1L;
+		} else if (key.IsSameAs(KEY_RPT1_VALIDATION)) {
+			val.ToLong(&temp1);
+			m_rpt1Validation = temp1 == 1L;
+		} else if (key.IsSameAs(KEY_GATEWAY_ADDRESS)) {
+			m_gatewayAddress = val;
+		} else if (key.IsSameAs(KEY_GATEWAY_PORT)) {
+			val.ToULong(&temp2);
+			m_gatewayPort = (unsigned int)temp2;
+		} else if (key.IsSameAs(KEY_LOCAL_ADDRESS)) {
+			m_localAddress = val;
+		} else if (key.IsSameAs(KEY_LOCAL_PORT)) {
+			val.ToULong(&temp2);
+			m_localPort = (unsigned int)temp2;
+		} else if (key.IsSameAs(KEY_DVAP_PORT)) {
+			m_port = val;
+		} else if (key.IsSameAs(KEY_DVAP_FREQUENCY)) {
+			val.ToULong(&temp2);
+			m_frequency = (unsigned int)temp2;
+		} else if (key.IsSameAs(KEY_DVAP_POWER)) {
+			val.ToLong(&temp1);
+			m_power = int(temp1);
+		} else if (key.IsSameAs(KEY_DVAP_SQUELCH)) {
+			val.ToLong(&temp1);
+			m_squelch = int(temp1);
+		} else if (key.IsSameAs(KEY_DVAP_OFFSET)) {
+			val.ToLong(&temp1);
+			m_offset = int(temp1);
+		} else if (key.IsSameAs(KEY_TIMEOUT)) {
+			val.ToULong(&temp2);
+			m_timeout = (unsigned int)temp2;
+		} else if (key.IsSameAs(KEY_ACK_TIME)) {
+			val.ToULong(&temp2);
+			m_ackTime = (unsigned int)temp2;
+		} else if (key.IsSameAs(KEY_BEACON_TIME)) {
+			val.ToULong(&temp2);
+			m_beaconTime = (unsigned int)temp2;
+		} else if (key.IsSameAs(KEY_BEACON_TEXT)) {
+			m_beaconText = val;
+		} else if (key.IsSameAs(KEY_BEACON_VOICE)) {
+			val.ToLong(&temp1);
+			m_beaconVoice = temp1 == 1L;
+		} else if (key.IsSameAs(KEY_LANGUAGE)) {
+			val.ToLong(&temp1);
+			m_language = TEXT_LANG(temp1);
+		} else if (key.IsSameAs(KEY_LOGGING)) {
+			val.ToLong(&temp1);
+			m_logging = temp1 == 1L;
+		} else if (key.IsSameAs(KEY_WINDOW_X)) {
+			val.ToLong(&temp1);
+			m_x = int(temp1);
+		} else if (key.IsSameAs(KEY_WINDOW_Y)) {
+			val.ToLong(&temp1);
+			m_y = int(temp1);
+		}
+
+		str = file.GetNextLine();
+	}
+
+	file.Close();
+}
+
+CDVAPNodeConfig::~CDVAPNodeConfig()
+{
+}
+
+#endif
+
+void CDVAPNodeConfig::getCallsign(wxString& callsign, wxString& gateway, DSTAR_MODE& mode, ACK_TYPE& ack, bool& restriction, bool& rpt1Validation) const
+{
+	callsign       = m_callsign;
+	gateway        = m_gateway;
+	mode           = m_mode;
+	ack            = m_ack;
+	restriction    = m_restriction;
+	rpt1Validation = m_rpt1Validation;
+}
+
+void CDVAPNodeConfig::setCallsign(const wxString& callsign, const wxString& gateway, DSTAR_MODE mode, ACK_TYPE ack, bool restriction, bool rpt1Validation)
+{
+	m_callsign       = callsign;
+	m_gateway        = gateway;
+	m_mode           = mode;
+	m_ack            = ack;
+	m_restriction    = restriction;
+	m_rpt1Validation = rpt1Validation;
+}
+
+void CDVAPNodeConfig::getNetwork(wxString& gatewayAddress, unsigned int& gatewayPort, wxString& localAddress, unsigned int& localPort) const
+{
+	gatewayAddress = m_gatewayAddress;
+	gatewayPort    = m_gatewayPort;
+	localAddress   = m_localAddress;
+	localPort      = m_localPort;
+}
+
+void CDVAPNodeConfig::setNetwork(const wxString& gatewayAddress, unsigned int gatewayPort, const wxString& localAddress, unsigned int localPort)
+{
+	m_gatewayAddress = gatewayAddress;
+	m_gatewayPort    = gatewayPort;
+	m_localAddress   = localAddress;
+	m_localPort      = localPort;
+}
+
+void CDVAPNodeConfig::getDVAP(wxString& port, unsigned int& frequency, int& power, int& squelch, int& offset) const
+{
+	port      = m_port;
+	frequency = m_frequency;
+	power     = m_power;
+	squelch   = m_squelch;
+	offset    = m_offset;
+}
+
+void CDVAPNodeConfig::setDVAP(const wxString& port, unsigned int frequency, int power, int squelch, int offset)
+{
+	m_port      = port;
+	m_frequency = frequency;
+	m_power     = power;
+	m_squelch   = squelch;
+	m_offset    = offset;
+}
+
+void CDVAPNodeConfig::getTimes(unsigned int& timeout, unsigned int& ackTime) const
+{
+	timeout = m_timeout;
+	ackTime = m_ackTime;
+}
+
+void CDVAPNodeConfig::setTimes(unsigned int timeout, unsigned int ackTime)
+{
+	m_timeout = timeout;
+	m_ackTime = ackTime;
+}
+
+void CDVAPNodeConfig::getBeacon(unsigned int& time, wxString& text, bool& voice, TEXT_LANG& language) const
+{
+	time     = m_beaconTime;
+	text     = m_beaconText;
+	voice    = m_beaconVoice;
+	language = m_language;
+}
+
+void CDVAPNodeConfig::setBeacon(unsigned int time, const wxString& text, bool voice, TEXT_LANG language)
+{
+	m_beaconTime  = time;
+	m_beaconText  = text;
+	m_beaconVoice = voice;
+	m_language    = language;
+}
+
+void CDVAPNodeConfig::getLogging(bool& logging) const
+{
+	logging = m_logging;
+}
+
+void CDVAPNodeConfig::setLogging(bool logging)
+{
+	m_logging = logging;
+}
+
+void CDVAPNodeConfig::getPosition(int& x, int& y) const
+{
+	x = m_x;
+	y = m_y;
+}
+
+void CDVAPNodeConfig::setPosition(int x, int y)
+{
+	m_x = x;
+	m_y = y;
+}
+
+#if defined(__WINDOWS__)
+
+bool CDVAPNodeConfig::write()
+{
+	m_config->Write(m_name + KEY_CALLSIGN, m_callsign);
+	m_config->Write(m_name + KEY_GATEWAY, m_gateway);
+	m_config->Write(m_name + KEY_MODE, long(m_mode));
+	m_config->Write(m_name + KEY_ACK, long(m_ack));
+	m_config->Write(m_name + KEY_RESTRICTION, m_restriction);
+	m_config->Write(m_name + KEY_RPT1_VALIDATION, m_rpt1Validation);
+	m_config->Write(m_name + KEY_GATEWAY_ADDRESS, m_gatewayAddress);
+	m_config->Write(m_name + KEY_GATEWAY_PORT, long(m_gatewayPort));
+	m_config->Write(m_name + KEY_LOCAL_ADDRESS, m_localAddress);
+	m_config->Write(m_name + KEY_LOCAL_PORT, long(m_localPort));
+	m_config->Write(m_name + KEY_DVAP_PORT, m_port);
+	m_config->Write(m_name + KEY_DVAP_FREQUENCY, long(m_frequency));
+	m_config->Write(m_name + KEY_DVAP_POWER, long(m_power));
+	m_config->Write(m_name + KEY_DVAP_SQUELCH, long(m_squelch));
+	m_config->Write(m_name + KEY_DVAP_OFFSET, long(m_offset));
+	m_config->Write(m_name + KEY_TIMEOUT, long(m_timeout));
+	m_config->Write(m_name + KEY_ACK_TIME, long(m_ackTime));
+	m_config->Write(m_name + KEY_BEACON_TIME, long(m_beaconTime));
+	m_config->Write(m_name + KEY_BEACON_TEXT, m_beaconText);
+	m_config->Write(m_name + KEY_BEACON_VOICE, m_beaconVoice);
+	m_config->Write(m_name + KEY_LANGUAGE, long(m_language));
+	m_config->Write(m_name + KEY_LOGGING, m_logging);
+	m_config->Write(m_name + KEY_WINDOW_X, long(m_x));
+	m_config->Write(m_name + KEY_WINDOW_Y, long(m_y));
+	m_config->Flush();
+
+	return true;
+}
+
+#else
+
+bool CDVAPNodeConfig::write()
+{
+	wxTextFile file(m_fileName.GetFullPath());
+
+	bool exists = file.Exists();
+	if (exists) {
+		bool ret = file.Open();
+		if (!ret) {
+			wxLogError(wxT("Cannot open the config file - %s"), m_fileName.GetFullPath().c_str());
+			return false;
+		}
+
+		// Remove the existing file entries
+		file.Clear();
+	} else {
+		bool ret = file.Create();
+		if (!ret) {
+			wxLogError(wxT("Cannot create the config file - %s"), m_fileName.GetFullPath().c_str());
+			return false;
+		}
+	}
+
+	wxString buffer;
+	buffer.Printf(wxT("%s=%s"), KEY_CALLSIGN.c_str(), m_callsign.c_str()); file.AddLine(buffer);
+	buffer.Printf(wxT("%s=%s"), KEY_GATEWAY.c_str(), m_gateway.c_str()); file.AddLine(buffer);
+	buffer.Printf(wxT("%s=%d"), KEY_MODE.c_str(), int(m_mode)); file.AddLine(buffer);
+	buffer.Printf(wxT("%s=%d"), KEY_ACK.c_str(), int(m_ack)); file.AddLine(buffer);
+	buffer.Printf(wxT("%s=%d"), KEY_RESTRICTION.c_str(), m_restriction ? 1 : 0); file.AddLine(buffer);
+	buffer.Printf(wxT("%s=%d"), KEY_RPT1_VALIDATION.c_str(), m_rpt1Validation ? 1 : 0); file.AddLine(buffer);
+	buffer.Printf(wxT("%s=%s"), KEY_GATEWAY_ADDRESS.c_str(), m_gatewayAddress.c_str()); file.AddLine(buffer);
+	buffer.Printf(wxT("%s=%u"), KEY_GATEWAY_PORT.c_str(), m_gatewayPort); file.AddLine(buffer);
+	buffer.Printf(wxT("%s=%s"), KEY_LOCAL_ADDRESS.c_str(), m_localAddress.c_str()); file.AddLine(buffer);
+	buffer.Printf(wxT("%s=%u"), KEY_LOCAL_PORT.c_str(), m_localPort); file.AddLine(buffer);
+	buffer.Printf(wxT("%s=%s"), KEY_DVAP_PORT.c_str(), m_port.c_str()); file.AddLine(buffer);
+	buffer.Printf(wxT("%s=%u"), KEY_DVAP_FREQUENCY.c_str(), m_frequency); file.AddLine(buffer);
+	buffer.Printf(wxT("%s=%d"), KEY_DVAP_POWER.c_str(), m_power); file.AddLine(buffer);
+	buffer.Printf(wxT("%s=%d"), KEY_DVAP_SQUELCH.c_str(), m_squelch); file.AddLine(buffer);
+	buffer.Printf(wxT("%s=%d"), KEY_DVAP_OFFSET.c_str(), m_offset); file.AddLine(buffer);
+	buffer.Printf(wxT("%s=%u"), KEY_TIMEOUT.c_str(), m_timeout); file.AddLine(buffer);
+	buffer.Printf(wxT("%s=%u"), KEY_ACK_TIME.c_str(), m_ackTime); file.AddLine(buffer);
+	buffer.Printf(wxT("%s=%u"), KEY_BEACON_TIME.c_str(), m_beaconTime); file.AddLine(buffer);
+	buffer.Printf(wxT("%s=%s"), KEY_BEACON_TEXT.c_str(), m_beaconText.c_str()); file.AddLine(buffer);
+	buffer.Printf(wxT("%s=%d"), KEY_BEACON_VOICE.c_str(), m_beaconVoice ? 1 : 0); file.AddLine(buffer);
+	buffer.Printf(wxT("%s=%d"), KEY_LANGUAGE.c_str(), int(m_language)); file.AddLine(buffer);
+	buffer.Printf(wxT("%s=%d"), KEY_LOGGING.c_str(), m_logging ? 1 : 0); file.AddLine(buffer);
+	buffer.Printf(wxT("%s=%d"), KEY_WINDOW_X.c_str(), m_x); file.AddLine(buffer);
+	buffer.Printf(wxT("%s=%d"), KEY_WINDOW_Y.c_str(), m_y); file.AddLine(buffer);
+
+	bool ret = file.Write();
+	if (!ret) {
+		file.Close();
+		wxLogError(wxT("Cannot write the config file - %s"), m_fileName.GetFullPath().c_str());
+		return false;
+	}
+
+	file.Close();
+
+	return true;
+}
+
+#endif
