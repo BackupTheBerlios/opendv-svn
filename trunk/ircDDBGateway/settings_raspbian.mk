6222f6f336a31664d7ef645e609e978f1e9f40f2
dl5di <dl5di@42a4f344-f963-4ded-a4f8-2595dd3c5695>
2013-01-21 22:57:27 +0000

    - makefile includes updated for amd64 - makefile includes added for Raspbian Wheezy (different from armhf) - minor fixes in start scripts

---
+++ trunk/ircDDBGateway/settings_raspbian.mk
@@ -0,0 +1,18 @@
+# Makefile-include
+#
+# Special settings for  Emdebian Compile for ARM processor, John Hays, June 2012
+# modified by Hans-J. Barthen, DL5DI 2012-11-04
+# Rename this file to settings.mk before you compile the source for the Debian armhf platform
+#
+	export DATADIR := "/home/opendv/data"
+	export LIBS    := $(shell wx-config --libs adv,core)
+	export CC      := $(shell wx-config --cxx)
+	export LDFLAGS := -g
+	export BINDIR  := "$(DESTDIR)/usr/bin"
+#
+# Comment out the first CFLAGS entry and uncomment the second to enable the DExtra Link. For DCS Link you must comment out the first
+# CFLAGS entry and uncomment the third entry. You must do a "make clean" before rebuilding the software.
+#
+	export CFLAGS  := -g -O2 -march=armv6 -mfpu=vfp -mfloat-abi=hard -Wall -Wno-non-virtual-dtor -Wno-strict-aliasing -Wno-psabi -DDATA_DIR='$(DATADIR)' -DBIN_DIR='$(BINDIR)'  -D_FILE_OFFSET_BITS=64 -D_LARGE_FILES -D__WXGTK__ -pthread $(shell wx-config --cxxflags)
+# 	export CFLAGS := -g -O2 -march=armv6 -mfpu=vfp -mfloat-abi=hard -Wall -Wno-non-virtual-dtor -Wno-strict-aliasing -Wno-psabi -DDATA_DIR='$(DATADIR)' -DBIN_DIR='$(BINDIR)' -D_FILE_OFFSET_BITS=64 -D_LARGE_FILES -D__WXGTK__ -pthread -DDEXTRA_LINK $(shell wx-config --cxxflags)
+# 	export CFLAGS := -g -O2 -march=armv6 -mfpu=vfp -mfloat-abi=hard -Wall -Wno-non-virtual-dtor -Wno-strict-aliasing -Wno-psabi -DDATA_DIR='$(DATADIR)' -DBIN_DIR='$(BINDIR)' -D_FILE_OFFSET_BITS=64 -D_LARGE_FILES -D__WXGTK__ -pthread -DDCS_LINK $(shell wx-config --cxxflags)
