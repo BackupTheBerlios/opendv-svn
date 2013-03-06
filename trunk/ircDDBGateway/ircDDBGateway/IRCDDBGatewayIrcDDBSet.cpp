867cb5c173f7bec0a7bf9f6a8b747c4918c08874
n8ohu <n8ohu@42a4f344-f963-4ded-a4f8-2595dd3c5695>
2013-03-06 23:54:12 +0000

    Misc updates

--- trunk/ircDDBGateway/ircDDBGateway/IRCDDBGatewayIrcDDBSet.cpp
+++ trunk/ircDDBGateway/ircDDBGateway/IRCDDBGatewayIrcDDBSet.cpp
@@ -53,6 +53,8 @@ m_password(NULL)
 	m_hostname->Append(wxT("stn570.dyndns.org"));
 	m_hostname->Append(wxT("75.76.90.90"));
 	m_hostname->Append(wxT("ircddb.dstar.su"));
+	m_hostname->Append(wxT("itsns3.no-ip.org"));
+	m_hostname->Append(wxT("k7ek.no-ip.org"));
 	sizer->Add(m_hostname, 0, wxALL | wxALIGN_LEFT, BORDER_SIZE);
 	m_hostname->SetStringSelection(hostname);
 
