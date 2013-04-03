41fbdec85ddd42bab1d2193be39bb0fae6c2eb3c
dl5di <dl5di@42a4f344-f963-4ded-a4f8-2595dd3c5695>
2013-04-03 22:05:33 +0000

    list of repository servers updated

--- trunk/ircDDBGateway/settings.mk
+++ trunk/ircDDBGateway/settings.mk
@@ -1,8 +1,8 @@
 # Makefile-include
 #
-# Settings for Raspbian Wheezy compile in qemu-debootstrap environment
-# Hans-J. Barthen, DL5DI 2013-01-21
-# Rename this file to settings.mk before you compile the source for the Raspbian platform
+# Build the ircDDB Gateway software on Linux with settings for Debian (i386)
+# 2012-09-07 Hans-J. Barthen, DL5DI 
+# Rename this file to settings.mk before you compile the source for the Debian platform
 #
 	export DATADIR := "/home/opendv/data"
 	export LIBS    := $(shell wx-config --libs adv,core)
@@ -11,8 +11,8 @@
 	export BINDIR  := "$(DESTDIR)/usr/bin"
 #
 # Comment out the first CFLAGS entry and uncomment the second to enable the DExtra Link. For DCS Link you must comment out the first
-# CFLAGS entry and uncomment the third entry. You must do a "make clean" before rebuilding the software.
+# CFLAGS entry and uncomment the thrird entrt. You must do a "make clean" before rebuilding the software.
 #
-	export CFLAGS  := -g -O2 -march=armv6 -mfpu=vfp -mfloat-abi=hard -Wall -Wno-non-virtual-dtor -Wno-strict-aliasing -Wno-psabi -DDATA_DIR='$(DATADIR)' -DBIN_DIR='$(BINDIR)'  -D_FILE_OFFSET_BITS=64 -D_LARGE_FILES -D__WXGTK__ -pthread $(shell wx-config --cxxflags)
-# 	export CFLAGS := -g -O2 -march=armv6 -mfpu=vfp -mfloat-abi=hard -Wall -Wno-non-virtual-dtor -Wno-strict-aliasing -Wno-psabi -DDATA_DIR='$(DATADIR)' -DBIN_DIR='$(BINDIR)' -D_FILE_OFFSET_BITS=64 -D_LARGE_FILES -D__WXGTK__ -pthread -DDEXTRA_LINK $(shell wx-config --cxxflags)
-# 	export CFLAGS := -g -O2 -march=armv6 -mfpu=vfp -mfloat-abi=hard -Wall -Wno-non-virtual-dtor -Wno-strict-aliasing -Wno-psabi -DDATA_DIR='$(DATADIR)' -DBIN_DIR='$(BINDIR)' -D_FILE_OFFSET_BITS=64 -D_LARGE_FILES -D__WXGTK__ -pthread -DDCS_LINK $(shell wx-config --cxxflags)
+	export CFLAGS := -g -O2 -m32 -Wall -Wno-non-virtual-dtor -Wno-strict-aliasing -DDATA_DIR='$(DATADIR)' -DBIN_DIR='$(BINDIR)' $(shell wx-config --cxxflags)
+#	export CFLAGS := -g -O2 -m32 -Wall -Wno-non-virtual-dtor -Wno-strict-aliasing -DDATA_DIR='$(DATADIR)' -DBIN_DIR='$(BINDIR)' -DDEXTRA_LINK $(shell wx-config --cxxflags)
+#	export CFLAGS := -g -O2 -m32 -Wall -Wno-non-virtual-dtor -Wno-strict-aliasing -DDATA_DIR='$(DATADIR)' -DBIN_DIR='$(BINDIR)' -DDCS_LINK $(shell wx-config --cxxflags)
