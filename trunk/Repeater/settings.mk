f01ddce8d32bbc2a91eb6524f635003db1cab193
dl5di <dl5di@42a4f344-f963-4ded-a4f8-2595dd3c5695>
2013-04-04 18:45:04 +0000

    makefile includes were messed up - fixed

--- trunk/Repeater/settings.mk
+++ trunk/Repeater/settings.mk
@@ -1,12 +1,10 @@
 # Makefile-include
 #
-# Build the ircDDB Gateway software on Debian Linux with default settings (x86)
-# Hans-J. Barthen, DL5DI, 2012-09-08
-# Rename this file to settings.mk before you compile the source for the Debian i386 platform
+# Build the ircDDB Gateway software on Linux with default settings (x86)
 #
-	export DATADIR := "/home/opendv/data"
-	export BINDIR  := $(DESTDIR)/usr/bin
+	export DATADIR := "/usr/local/etc"
+	export BINDIR  := "/usr/local/bin"
 	export CC      := $(shell wx-config --cxx)
 	export LDFLAGS := -g
-	export CFLAGS  := -g -O2 -m32 -Wall -Wno-non-virtual-dtor -Wno-strict-aliasing -DDATA_DIR='$(DATADIR)' -DBIN_DIR='$(BINDIR)' $(shell wx-config --cxxflags)
+	export CFLAGS  := -g -O2 -Wall -Wno-non-virtual-dtor -Wno-strict-aliasing -DDATA_DIR='$(DATADIR)' -DBIN_DIR='$(BINDIR)' $(shell wx-config --cxxflags)
 	export LIBS := -lportaudio -lusb-1.0 $(shell wx-config --libs adv,core)
