ec8214b0eb0bf008f9839937402576ec62ad1168
naylorjs <naylorjs@42a4f344-f963-4ded-a4f8-2595dd3c5695>
2013-08-31 09:41:52 +0000

    The 20130831 beta release of the Repeater package.

--- trunk/Repeater/Common/Version.h
+++ trunk/Repeater/Common/Version.h
@@ -26,9 +26,9 @@ const wxString VENDOR_NAME = wxT("G4KLX");
 const wxString SVNREV = wxT("$Revision$ on $Date$");
 
 #if defined(__WXDEBUG__)
-const wxString VERSION = wxT("20130726 - DEBUG");
+const wxString VERSION = wxT("20130831 - DEBUG");
 #else
-const wxString VERSION = wxT("20130726");
+const wxString VERSION = wxT("20130831");
 #endif
 
 #endif
