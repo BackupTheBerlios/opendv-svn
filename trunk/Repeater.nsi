6d778a1443ca955ff1b6ff25a7c99d8df584bc69
naylorjs <naylorjs@42a4f344-f963-4ded-a4f8-2595dd3c5695>
2013-09-03 16:03:17 +0000

    The 20130903 beta release of the Repeater package.

--- trunk/Repeater.nsi
+++ trunk/Repeater.nsi
@@ -11,8 +11,8 @@
 ;Configuration
 
   ;General
-  Name "Repeater 20130831"
-  OutFile "Repeater-20130831.exe"
+  Name "Repeater 20130903"
+  OutFile "Repeater-20130903.exe"
 
   ;Folder selection page
   InstallDir "$PROGRAMFILES\Repeater"
--- trunk/Repeater/Common/Version.h
+++ trunk/Repeater/Common/Version.h
@@ -26,9 +26,9 @@ const wxString VENDOR_NAME = wxT("G4KLX");
 const wxString SVNREV = wxT("$Revision$ on $Date$");
 
 #if defined(__WXDEBUG__)
-const wxString VERSION = wxT("20130831 - DEBUG");
+const wxString VERSION = wxT("20130903 - DEBUG");
 #else
-const wxString VERSION = wxT("20130831");
+const wxString VERSION = wxT("20130903");
 #endif
 
 #endif
